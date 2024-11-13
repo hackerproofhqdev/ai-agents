832384b5e78c3ea6a63b66e0519e5411f3ac8e3f
unknown <hammadrehman208@gmai.com>
2024-11-14 00:18:26 +0500

    Adding More Options in resume Agent

--- quiz_agent.py
+++ quiz_agent.py
@@ -1,44 +1,51 @@
 from typing import TypedDict
 from langchain_openai import ChatOpenAI
-from langgraph.graph import StateGraph  , START , END
-from langchain_core.pydantic_v1 import BaseModel , Field
+from langgraph.graph import StateGraph, START, END
+from langchain_core.pydantic_v1 import BaseModel, Field
 from langchain_core.prompts import ChatPromptTemplate
 
+
 class Question(BaseModel):
-  """Question Model"""
-  question : str = Field(description="Question to be answered")
-  answer : str = Field(description="Correct answer")
-  options : list[str] = Field(description="Options to choose from")
+    """Question Model"""
+
+    question: str = Field(description="Question to be answered")
+    answer: str = Field(description="Correct answer")
+    options: list[str] = Field(description="Options to choose from")
 
 
 class Quiz(BaseModel):
-  """Quiz Model"""
-  questions : list[Question] = Field(description="List of questions")
+    """Quiz Model"""
+
+    questions: list[Question] = Field(description="List of questions")
 
 
 class State(TypedDict):
-  """State Model"""
-  quiz : Quiz
-  job_description : str
+    """State Model"""
+
+    quiz: Quiz
+    job_description: str
 
 
+def llm_node(state: State):
+    llm = ChatOpenAI(model="gpt-4o-mini", temperature=0.5)
+    llm_json = llm.with_structured_output(Quiz)
+    messages = [
+        (
+            "system",
+            "You Are the Interviwer .  You have to  Make  Quiz From for User according to Job Description",
+        ),
+        ("human", "Here is the Job Desc : \n {job_description}"),
+    ]
+    messages_template = ChatPromptTemplate.from_messages(messages)
+    chain = messages_template | llm_json
+    quiz = chain.invoke({"job_description": state["job_description"]})
+    return {"quiz": quiz}
 
-def llm_node(state:State):
-  llm = ChatOpenAI(model="gpt-4o-mini" , temperature=0.5)
-  llm_json = llm.with_structured_output(Quiz)
-  messages = [
-      ("system" , "You Are the Interviwer .  You have to Take the Make  Quiz From for User according to Job Description"),
-      ("human" , "Here is the Job Desc : \n {job_description}")
-  ] 
-  messages_template = ChatPromptTemplate.from_messages(messages)
-  chain = messages_template | llm_json
-  quiz = chain.invoke({"job_description":state['job_description']})
-  return {"quiz":quiz}
 
 builder = StateGraph(State)
 
-builder.add_node("llm_node" , llm_node)
-builder.add_edge(START , "llm_node")
-builder.add_edge("llm_node" , END)
+builder.add_node("llm_node", llm_node)
+builder.add_edge(START, "llm_node")
+builder.add_edge("llm_node", END)
 
 graph = builder.compile()
--- resume_agent.py
+++ resume_agent.py
@@ -1,50 +1,79 @@
 from typing import Optional
-from langgraph.graph import StateGraph  , START , END , add_messages , MessagesState
+from langgraph.graph import StateGraph, START, END, MessagesState
 from langchain_openai import ChatOpenAI
-from pydantic import BaseModel , Field
+from pydantic import BaseModel, Field
 
 
 llm = ChatOpenAI(model="gpt-4o-mini")
 
+
 class Skills(BaseModel):
     """Skilles Model"""
-    skill_name : str = Field(description="Skill Name")
-    description : str = Field(description="Skill description")
+
+    skill_name: str = Field(description="Skill Name")
+    description: str = Field(description="Skill description")
 
 
 class PastExperience(BaseModel):
     """PastExperience Model"""
-    postion : str = Field(description="Role Work At")
-    performed_for : Optional[str] = Field(default=None,description="Name Of the Company or Personal")
-    roles : Optional[list[str]] = Field(default=None , description="Detail Roles and very things performed") 
-    start_date : str = Field(description="Date Of Started")
-    end_date : str = Field(description="Date of End")
+
+    postion: str = Field(description="Role Work At")
+    performed_for: Optional[str] = Field(
+        default=None, description="Name Of the Company or Personal"
+    )
+    roles: Optional[list[str]] = Field(
+        default=None, description="Detail Roles and very things performed"
+    )
+    start_date: str = Field(description="Date Of Started")
+    end_date: str = Field(description="Date of End")
 
 
 class Contact(BaseModel):
     """Contact Info Model"""
-    phone_number :str = Field(description="Phone Number of resumer")
-    email : str = Field(description="Email of Resumer")
+
+    phone_number: str = Field(description="Phone Number of resumer")
+    email: str = Field(description="Email of Resumer")
+
 
 class Education(BaseModel):
     """Education Model"""
-    degree : str = Field(description="Degress Details")
-    completed_at : str = Field(description="Complete Date")
-    platform_name : str = Field(description="Name Of Platform")
+
+    degree: str = Field(description="Degress Details")
+    completed_at: str = Field(description="Complete Date")
+    platform_name: str = Field(description="Name Of Platform")
+
+
+class Certification(BaseModel):
+    """Certification Model"""
+
+    name: str = Field(description="Name Of the Certification")
+    issued_at: Optional[str] = Field(description="The Date Of Issue")
+
+
+class Awards(BaseModel):
+    """Award Model"""
+
+    award: str = Field(description="Award Name")
+
 
 class Resume(BaseModel):
     """Resume Model"""
-    name : str = Field(description="Name of The Resumer")
-    role : str = Field(description="Role of the Resumer")
-    about : str = Field(description="Person About")
-    skills  : list[Skills] = Field(description="List of Skills")
-    past_experince :  list [PastExperience] = Field(description="List Of past_skills")
-    education_info : list[Education]
-    contact_info : Contact
+
+    name: str = Field(description="Name of The Resumer")
+    role: str = Field(description="Role of the Resumer")
+    about: str = Field(description="Person About")
+    skills: list[Skills] = Field(description="List of Skills")
+    past_experince: Optional[list[PastExperience]] = Field(
+        description="List Of Experience Including Projects"
+    )
+    education_info: Optional[list[Education]] = Field(description="Education Info")
+    contact_info: Contact = Field(description="Contact Information")
+    certifications: Optional[list[Certification]] = Field(description="Certifications")
+    awards: Optional[list[Awards]] = Field(description="List Of Awards")
 
 
 class State(MessagesState):
-    resume : Resume
+    resume: Resume
 
 
 llm_with_json = llm.with_structured_output(Resume)
@@ -53,20 +82,20 @@ builder = StateGraph(State)
 
 
 system_message = (
-        "system",
-        "You are an AI assistant specialized in creating professional resumes. Given the user's information, generate a  resume that highlights their qualifications, experience, and skills. Ensure the resume is clear and concise make sure it is also analyze By ATS Systems. Do not include any additional commentary or text outside of the resume itself. Only output the resume."
+    "system",
+    "You are an AI assistant specialized in creating professional resumes. Given the user's information, generate a  resume that highlights their qualifications, experience, and skills. Ensure the resume is clear and concise make sure it is also analyze By ATS Systems. Do not include any additional commentary or text outside of the resume itself. Only output the resume.",
 )
 
 
-def llm_node(state:State):
-    
+def llm_node(state: State):
+
     messages = [system_message] + state["messages"]
-    return {"messages":messages , "resume":llm_with_json.invoke(messages)}
+    return {"messages": messages, "resume": llm_with_json.invoke(messages)}
 
 
-builder.add_node("llm_node" , llm_node)
+builder.add_node("llm_node", llm_node)
 
-builder.add_edge(START , "llm_node")
-builder.add_edge("llm_node" , END)
+builder.add_edge(START, "llm_node")
+builder.add_edge("llm_node", END)
 
 graph = builder.compile()
